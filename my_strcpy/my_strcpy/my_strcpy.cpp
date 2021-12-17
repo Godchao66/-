dd70074be3686d73c0aaf3c2df43e1b2e6ff128a
Godchao66 <90696318+Godchao66@users.noreply.github.com>
2021-12-17 15:09:10 +0800

    Add files via upload

---
+++ my_strcpy/my_strcpy/my_strcpy.cpp
@@ -0,0 +1,22 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+void my_strcpy(char* dest, char* sour)
+{
+	while (*dest++ = *sour++)
+	{
+		;
+	}
+
+}
+int main()
+{
+	char arr1[100];
+	char arr2[200];
+	gets_s(arr2);
+	my_strcpy(arr1, arr2);
+	printf("%s", arr1);
+	return 0;
+
+
+}
+//'\0'µÄACSLLÊÇ0
